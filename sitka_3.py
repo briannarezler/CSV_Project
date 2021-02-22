5b7b9a883cd2bcd4b2ba776f14ea4efcc23ee125
briannarezler <brianna_rezler1@baylor.edu>
2021-02-22 13:47:29 -0600

    worked on sitka in class

--- sitka_3.py
+++ sitka_3.py
@@ -1,7 +1,12 @@
+# change file to include all of 2018
+# chagne title to daily and low and high temperatures
+# extract low temps and add them
+# shade areas between highs and lows
+
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("sitka_weather_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
@@ -14,6 +19,7 @@ for index, column_header in enumerate(header_row):
     print("Index:", index, "Column Name:", column_header)
 
 highs = []
+lows = []
 dates = []
 
 # as an example
@@ -23,6 +29,7 @@ dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    lows.append(int(row[6]))
     converted_date = datetime.strptime(row[2], "%Y-%m-%d")
     dates.append(converted_date)
 
@@ -35,12 +42,15 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
 
 fig.autofmt_xdate()
 
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
+
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 plt.show()
