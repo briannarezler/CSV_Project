a8d4aa1bcca9bbb1a01fa71c940a85e6a2166175
briannarezler <brianna_rezler1@baylor.edu>
2021-02-25 11:19:43 -0600

    finished subplots

--- sitka_4.py
+++ sitka_4.py
@@ -1,17 +1,17 @@
-#We hard coded the indexes corresponding to the TMIN and TMAX
-#columns. Use the header row to determine the indexes for these values, so your program can work
-#for Sitka or Death Valley. Use the station name to automatically generate an appropriate title
-#for your graph as well.
+# We hard coded the indexes corresponding to the TMIN and TMAX
+# columns. Use the header row to determine the indexes for these values, so your program can work
+# for Sitka or Death Valley. Use the station name to automatically generate an appropriate title
+# for your graph as well.
 
-create 2 subplot graphs in one visualization so you can see both graphs to compare side by side.
+# create 2 subplot graphs in one visualization so you can see both graphs to compare side by side.
 
 import csv
 from datetime import datetime
 
-open_file = "sitka_weather_2018_simple.csv"
-open_file = "death_valley_2018_simple.csv"
-place_name = ""
-with open(open_file) as c:
+open_sk_file = "sitka_weather_2018_simple.csv"
+
+sk_place_name = ""
+with open(open_sk_file) as c:
     csv_file = csv.reader(c)
     header_row = next(csv_file)
 
@@ -21,35 +21,68 @@ with open(open_file) as c:
     date_pos = header_row.index("DATE")
     name_pos = header_row.index("NAME")
 
-    highs = []
-    lows = []
-    dates = []
+    sk_highs = []
+    sk_lows = []
+    sk_dates = []
 
     for row in csv_file:
         try:
             high = int(row[high_pos])
             low = int(row[low_pos])
             converted_date = datetime.strptime(row[date_pos], "%Y-%m-%d")
-            place_name = row[name_pos]
+            sk_place_name = row[name_pos]
         except ValueError:
             print(f"missing data for {converted_date}")
         else:
-            highs.append(int(row[high_pos]))
-            lows.append(int(row[low_pos]))
-            dates.append(converted_date)
+            sk_highs.append(int(row[high_pos]))
+            sk_lows.append(int(row[low_pos]))
+            sk_dates.append(converted_date)
+
+open_dv_file = "death_valley_2018_simple.csv"
+dv_place_name = ""
+with open(open_dv_file) as c:
+    csv_file = csv.reader(c)
+    header_row = next(csv_file)
+
+    print(header_row)
+    high_pos = header_row.index("TMAX")
+    low_pos = header_row.index("TMIN")
+    date_pos = header_row.index("DATE")
+    name_pos = header_row.index("NAME")
+
+    dv_highs = []
+    dv_lows = []
+    dv_dates = []
+
+    for row in csv_file:
+        try:
+            high = int(row[high_pos])
+            low = int(row[low_pos])
+            converted_date = datetime.strptime(row[date_pos], "%Y-%m-%d")
+            dv_place_name = row[name_pos]
+        except ValueError:
+            print(f"missing data for {converted_date}")
+        else:
+            dv_highs.append(int(row[high_pos]))
+            dv_lows.append(int(row[low_pos]))
+            dv_dates.append(converted_date)
 
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(2)
-plt.title(f"Daily high and low temperatures - 2018 {place_name}", fontsize=16)
+plt.suptitle(
+    f"Temperature comparison between {sk_place_name} and {dv_place_name}", fontsize=16
+)
 
-ax[0].plot(dates, highs, c="red")
-ax[0].plot(dates, lows, c="blue")
-plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+ax[0].plot(sk_dates, sk_highs, c="red")
+ax[0].plot(sk_dates, sk_lows, c="blue")
+ax[0].title.set_text(f"{sk_place_name}")
+ax[0].fill_between(sk_dates, sk_highs, sk_lows, facecolor="blue", alpha=0.1)
 
-ax[1].plot(dates, highs, c="red")
-ax[1].plot(dates, lows, c="blue")
-plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+ax[1].plot(dv_dates, dv_highs, c="red")
+ax[1].plot(dv_dates, dv_lows, c="blue")
+ax[1].title.set_text(f"{dv_place_name}")
+ax[1].fill_between(dv_dates, dv_highs, dv_lows, facecolor="blue", alpha=0.1)
 
 plt.xlabel("", fontsize=12)
 fig.autofmt_xdate()
